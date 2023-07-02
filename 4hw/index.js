f5c64912abc7a3baa2c115aeb69899d31818c7ca
Kateryna <shust.kateryna0@gmail.com>
2023-07-02 19:50:49 +0200

    fourthHomework

---
+++ 4hw/index.js
@@ -0,0 +1,52 @@
+//Вивести на сторінку в один рядок через кому числа від 10 до 20//
+let result = "";
+for (let i = 10; i <= 20; i++) {
+    result += i + ", ";
+}
+    console.log(result);
+
+//Вивести квадрати чисел від 10 до 20//
+for (let i = 10; i <= 20; i++) 
+
+    console.log(i * i);
+
+//Вивести таблицю множення на 7//
+for (let i = 1; i <= 10; i++) {
+    console.log(7 * i);
+}
+
+//Знайти суму всіх цілих чисел від 1 до 15//
+let sum = 0;
+for (let i = 1; i <= 15; i++) {
+    sum += i;
+}
+    console.log(sum);
+
+//Знайти добуток усіх цілих чисел від 15 до 35//
+let product = 1;
+for (let i = 15; i <= 35; i++) {
+    product *= i;
+}
+console.log(product);
+
+//Знайти середнє арифметичне всіх цілих чисел від 1 до 500//
+let sum = 0;
+for (let i = 1; i <= 500; i++) {
+    sum += i;
+}
+let average = sum / 500;
+
+    console.log(average);
+
+//Вивести суму лише парних чисел в діапазоні від 30 до 80//
+let sum = 0;
+for (let i = 30; i <= 80; i += 2) {
+    sum += i;
+}
+
+    console.log(sum);
+
+//Вивести всі числа в діапазоні від 100 до 200 кратні 3//
+for (let i = 100; i <= 200; i += 3) 
+
+    console.log(i);
