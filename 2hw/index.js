31614caf75ab9707ba4582e551964e236cbe6c21
Kateryna <shust.kateryna0@gmail.com>
2023-07-02 18:13:42 +0200

    secondHomework

---
+++ 2hw/index.js
@@ -0,0 +1,31 @@
+const operator = prompt("Enter the function (+, -, *, /):");
+const num1 = Number(prompt("Enter the first number:"));
+const num2 = Number(prompt("Enter the second number:"));
+
+const result;
+
+if (isNaN(num1) || isNaN(num2)) {
+    alert("Invalid input");
+} else {
+    if (operator === "+") {
+        result = num1 + num2;
+    } else if (operator === "-") {
+        result = num1 - num2;
+    } else if (operator === "*") {
+        result = num1 * num2;
+    } else if (operator === "/") {
+    if (num2 === 0) {
+        alert("Cannot divide by zero");
+    } else {
+        result = num1 / num2;
+    }
+    } else {
+    alert("Invalid operator");
+    }
+}
+
+if (result !== undefined) {
+    let summary = "Summary: " + num1 + operator + num2 + "=" + result;
+
+    alert(summary);
+}
